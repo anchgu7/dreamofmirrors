b3178275fce9f4a799d2846310527dd8f93ce7ec
Francis Scipik Tang <fltang@ucsc.edu>
2014-05-02 21:15:23 -0700

    Start of Prototype
    Got the project set up. Right now I have a crappy mouse drag motion for rotating the mirror
    
    The main issue I have right now is having the sun rays work
    I've tried particles, but there some odd collision with that
    I've thought of something simple like a sunlight ray sprite and scaling it to match what it should do, I'll try to get that in for the prototype

---
+++ New folder/Assets/Scripts/Game/MirrorController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MirrorController : MonoBehaviour {
+
+	public float rotationSpeed;
+	
+	private bool rotatingObject = false;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pz.z = 0;
+		RaycastHit2D hit = Physics2D.Raycast(pz, Vector2.zero);
+		if(rotatingObject) {
+			transform.rotation = Quaternion.Euler(0.0f, 0.0f, 57 * Mathf.Atan2((pz.y - transform.position.y), (pz.x - transform.position.x)));
+		}
+		
+		// Checks for mouseclick while over object
+		if (Input.GetMouseButtonDown (0)) {
+			if(hit.collider != null) {
+				rotatingObject = true;
+			}
+		} else if (Input.GetMouseButtonUp (0)){
+			rotatingObject = false;
+		}
+	}
+}
