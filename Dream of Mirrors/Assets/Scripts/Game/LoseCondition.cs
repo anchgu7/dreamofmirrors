8afaaee76b1d7eb6dc3a0aa3da2238b40c366e29
Francis Scipik Tang <fltang@ucsc.edu>
2014-05-10 11:37:58 -0700

    Lose Condition
    Added in lose condition and screen
    Removed the unnecessary files

---
+++ Dream of Mirrors/Assets/Scripts/Game/LoseCondition.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCondition : MonoBehaviour {
+	void Update() {
+		Object[] settlements = GameObject.FindGameObjectsWithTag ("Settlement");
+		if(settlements.Length == 0) {
+			Application.LoadLevel (Application.loadedLevel + 1);
+		}
+	}
+}
---
+++ Dream of Mirrors/Assets/Scripts/Menu/BackButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour {
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2 + 50, 100, 50), "Back")) {
+			Application.LoadLevel (0);
+		}
+	}
+}
