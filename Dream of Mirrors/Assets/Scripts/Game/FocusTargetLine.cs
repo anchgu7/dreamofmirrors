7c6c0904878c10495ba025598994aef806ddb46a
SeanMendenhall <semenden@ucsc.edu>
2014-05-15 12:48:55 -0700

    Changed FocusTargetLine

---
+++ Dream of Mirrors/Assets/Scripts/Game/FocusTargetLine.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FocusTargetLine : MonoBehaviour {
+
+	bool visible = false;
+	Color color;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (visible) 
+		{
+			color = renderer.material.color;
+			if (color.a < 255)
+				color.a = 255;
+			renderer.material.color = color;
+		}
+		else
+		{
+			color = renderer.material.color;
+			if (color.a > 0)
+				color.a = 0;
+			renderer.material.color = color;
+		}
+	}
+}
