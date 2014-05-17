64a26b5bfd0be51fdf4e906c2add424b9d7eecc1
Francis Scipik Tang <fltang@ucsc.edu>
2014-05-17 15:31:00 -0700

    UI and clean-up
    I changed up the UI a bit and got some new font.
    Cleaned up the folders a bit, removing art that won't be used anymore.

--- Dream of Mirrors/Assets/Scripts/Game/LoseCondition.cs
+++ Dream of Mirrors/Assets/Scripts/Game/LoseCondition.cs
@@ -5,18 +5,29 @@ public class LoseCondition : MonoBehaviour {
 
 	public GameObject timer, settlement;
 
+	private int year, day, hour;
+
 	void Update() {
 		if (!GameFields.paused) {
 			GameFields.gameTime += Time.deltaTime;
+			
+			year = (int) GameFields.gameTime;
+			day = year % 365;
+			year /= 365;
+			
+			hour = day % 60;
+			day /= 60;
+			
+			hour /= 3;
 		
-			timer.GetComponent<TextMesh>().text = "You have lasted:\n" + (int) (GameFields.gameTime / 3) + " hours";
+			timer.GetComponent<TextMesh>().text = "Time Passed:\n" + year + " year(s)\n" + day + " day(s)\n" + hour + " hour(s)";
 	
 			Object[] settlements = GameObject.FindGameObjectsWithTag ("Settlement");
 			if(settlements.Length == 0) {
 				Application.LoadLevel (Application.loadedLevel + 1);
 			}
 		
-			settlement.GetComponent<TextMesh>().text = "Settlements:\n" + settlements.Length;
+			settlement.GetComponent<TextMesh>().text = "Settlements\nRemaining:\n" + settlements.Length;
 		}
 	}
 	
